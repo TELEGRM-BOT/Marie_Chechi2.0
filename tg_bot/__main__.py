beb2eb6efab3ab5336dc4a20fd1f2103ad357090
abhianeesh <59135313+abhianeesh@users.noreply.github.com>
2020-04-20 11:37:28 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -142,7 +142,8 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN)
     else:
-        update.effective_message.reply_text("ഹായ് മച്ചാൻ,  എന്താണ്??")
+        update.effective_message.reply_text("ഹായ്, ന്തെല്ലാ.. 😉")
+
 
 
 # for test purposes
